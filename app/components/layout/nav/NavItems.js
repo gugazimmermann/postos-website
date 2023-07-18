fa8c5a6ffad28312f3b6452ba2682ba32b5fa9d7
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-18 14:25:48 -0300

    active route

--- app/components/layout/nav/NavItems.js
+++ app/components/layout/nav/NavItems.js
@@ -1,14 +1,24 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
 const NavItems = () => {
-  const NavItem = ({ to, text }) => (
-    <Link
-      href={to}
-      className='flex cursor-pointer text-slate-800 hover:text-amber-500 hover:font-bold transition-colors duration-300'
-    >
-      {text}
-    </Link>
-  );
+  const pathname = usePathname();
+
+  const NavItem = ({ to, text }) => {
+    const isActive = pathname.startsWith(to);
+    return (
+      <Link
+        href={to}
+        className={`flex cursor-pointer ${
+          isActive ? 'text-amber-500 font-bold' : 'text-slate-800 hover:text-amber-500 hover:font-bold transition-colors duration-300'
+        }`}
+      >
+        {text}
+      </Link>
+    );
+  };
 
   return (
     <>
