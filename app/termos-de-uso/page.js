b4d7f5adb888daaa1f27e1ccf61c061d7a81170f
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-18 14:14:05 -0300

    usage terms

--- app/termos-de-uso/page.js
+++ app/termos-de-uso/page.js
@@ -1,3 +1,11 @@
-export default function UsageTems() {
-  return <h1>Termos de Uso!</h1>
+import { Section, SectionTitle } from '../components/layout/sections';
+import { UsageTermsText } from '../components/pages/usage-terms';
+
+export default function UsageTerms() {
+  return (
+    <Section>
+      <SectionTitle title='Termos de Uso' />
+      <UsageTermsText />
+    </Section>
+  );
 }
