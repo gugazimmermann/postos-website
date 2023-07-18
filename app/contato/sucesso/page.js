31d888db2a5157384c3cb4241aa2b26218c11e76
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-18 14:26:03 -0300

    contact us success

---
+++ app/contato/sucesso/page.js
@@ -0,0 +1,11 @@
+import { Section, SectionTitle, SectionParagraph } from '../../components/layout/sections';
+export default function ContactUsSuccess() {
+  return (
+    <Section>
+      <SectionTitle title='Mensagem Enviada' />
+      <SectionParagraph>
+        Sua mensagem foi enviada com sucesso, entraremos em contato em breve.
+      </SectionParagraph>
+    </Section>
+  );
+}
