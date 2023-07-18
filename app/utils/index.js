44b0687b109f55df4d45cd3c436fa61e66e28474
Guga Zimmermann <gugazimmermann@gmail.com>
2023-07-18 15:33:32 -0300

    br states

--- app/utils/index.js
+++ app/utils/index.js
@@ -3,3 +3,4 @@ export { default as isValidEmail } from './is-valid-email';
 export { default as faq } from './faq';
 export * as api from './api';
 export * as masks from './masks';
+export { default as statesBR } from './states-br';
