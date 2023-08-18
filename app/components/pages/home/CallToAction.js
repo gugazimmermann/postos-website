839f2bde2bd01d3f504656ba32aeb55e50291e98
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-18 02:51:49 -0300

    changes

--- app/components/pages/home/CallToAction.js
+++ app/components/pages/home/CallToAction.js
@@ -5,8 +5,8 @@ const CallToAction = () => {
   return (
     <section className='container flex flex-col bg-slate-50 rounded-3xl shadow-lg'>
       <div className='py-8 px-4 flex flex-col justify-center items-center uppercase'>
-        <h2 className='text-center mb-4 text-3xl tracking-tight font-extrabold leading-tight text-slate-800'>
-          Modernize seu Posto Hoje!
+        <h2 className='text-amber-500 text-center mb-4 text-3xl tracking-tight font-extrabold leading-tight'>
+          Leve a Modernidade ao Seu Posto!
         </h2>
         <p className='mb-6 text-slate-800 text-center'>
           Faça sua conta no {process.env.NEXT_PUBLIC_TITLE} Gratuitamente.
--- app/components/pages/home/WhatWeDo.js
+++ app/components/pages/home/WhatWeDo.js
@@ -14,52 +14,50 @@ const WhatWeDoCard = ({ image, title, text }) => {
 
 const WhatWeDo = () => {
   return (
-    <section className='container my-8 space-y-4 md:space-y-8 text-slate-800'>
+    <section className='container my-4 space-y-4 text-slate-800'>
       <div className='w-full'>
         <h2 className='text-center md:text-left text-3xl font-bold'>O Que Oferecemos</h2>
         <p className='mt-4 text-center md:text-left text-slate-500'>
-          A {process.env.NEXT_PUBLIC_TITLE} oferece um sistema altamente especializado para o
-          controle de abastecimentos de veículos de frota em postos de combustíveis.
-          Diferenciando-se de sistemas genéricos, não nos envolvemos em funções como controle de
-          estoque ou aspectos fiscais.
+          A {process.env.NEXT_PUBLIC_TITLE} é especialista em oferecer soluções focadas no controle
+          de abastecimentos de veículos de frotas em postos de combustíveis. Concentramo-nos no que
+          fazemos de melhor, permitindo que sua operação de abastecimento seja otimizada sem as
+          complexidades adicionais de gestão de notas fiscais, estoque ou impostos.
         </p>
       </div>
-
-      <div className='mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3'>
+      <div className='mt-4 grid gap-4 grid-cols-3'>
         <WhatWeDoCard
           image='/images/what-we-do/facial_recognition.png'
           title='Segurança Aprimorada'
-          text='No momento do abastecimento, priorizamos a segurança. Os motoristas têm a opção de utilizar o Reconhecimento Facial ou a Assinatura Digital, garantindo um processo altamente seguro e confiável.'
+          text='Durante o abastecimento, destacamos a segurança como prioridade. Oferecemos opções modernas de Reconhecimento Facial, Biometria e Assinatura Digital, proporcionando um processo robusto e confiável.'
         />
-
         <WhatWeDoCard
           image='/images/what-we-do/email.png'
-          title='Transparência em Tempo Real'
-          text='A cada abastecimento realizado, nosso sistema notifica imediatamente o cliente. Através de e-mails em tempo real, fornecemos informações detalhadas sobre o abastecimento, incluindo dados do veículo, motorista, posto, valor e quantidade abastecida.'
+          title='Notificações Instantâneas'
+          text='A cada abastecimento, informamos o cliente em tempo real. E-mails detalhados fornecem todas as informações pertinentes, incluindo veículo, motorista, posto e dados de abastecimento.'
         />
 
         <WhatWeDoCard
           image='/images/what-we-do/invoice.png'
-          title='Agilidade e Controle Simplificado'
-          text='Simplifique o processo de gerenciamento. Receba notificações automáticas sobre datas de fechamento de faturas para cada cliente. Com alguns cliques, tenha controle total e visualize o status dos pagamentos.'
+          title='Gerenciamento Ágil e Descomplicado'
+          text='Nosso sistema facilita a gestão. Receba alertas automáticos sobre fechamentos de fatura e, com apenas alguns cliques, mantenha-se atualizado sobre o status dos pagamentos'
         />
 
         <WhatWeDoCard
           image='/images/what-we-do/clients.png'
-          title='Gestão de Clientes Aperfeiçoada'
-          text='Tenha o controle sobre os seus clientes frotistas enquanto oferece maior autonomia a eles. Lembre-se de que os dados de cadastro de veículos e motoristas são de responsabilidade do cliente, não do posto.'
+          title='Gestão de Clientes Evoluída'
+          text='Gerencie seus clientes frotistas com eficiência e ofereça a eles a autonomia necessária. O cadastro de veículos e motoristas é simplificado e mantido pelos próprios clientes, otimizando seu processo.'
         />
 
         <WhatWeDoCard
           image='/images/what-we-do/vehicles.png'
-          title='Detalhes de Veículos Precisos'
-          text='Além das informações básicas, nosso sistema exibe detalhes como marca, modelo e ano de cada veículo. Isso inclui a capacidade de verificar quais tipos de combustíveis podem ser usados em postos específicos.'
+          title='Informações Detalhadas de Veículos'
+          text='Nosso sistema não só fornece dados básicos, mas também detalha marca, modelo e ano dos veículos, incluindo a capacidade de verificar compatibilidade de combustíveis para cada veículo em postos específicos.'
         />
 
         <WhatWeDoCard
           image='/images/what-we-do/drivers.png'
-          title='Verificação de Motoristas Simplificada'
-          text='Está autorizado? Nossos recursos permitem verificar se um motorista tem permissão para abastecer em determinado posto ou acesso a um produto específico.'
+          title='Verificação Simplificada de Motoristas'
+          text='Nosso sistema possibilita a rápida verificação da autorização de um motorista para abastecer em determinados postos ou para ter acesso a produtos específicos.'
         />
       </div>
     </section>
