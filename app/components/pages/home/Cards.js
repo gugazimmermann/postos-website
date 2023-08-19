df5b12c00faadaca2fd58c80aacc8dd7c74bc757
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-19 12:45:18 -0300

    fix to tablet

--- app/components/pages/home/Cards.js
+++ app/components/pages/home/Cards.js
@@ -16,7 +16,7 @@ const Card = ({ image, title, text }) => {
 
 const Cards = () => {
   return (
-    <section className='container mt-4 px-4 md:px-16 grid gap-8 md:gap-4 grid-cols-1 md:grid-cols-3'>
+    <section className='container mt-4 px-4 lg:px-16 grid gap-8 lg:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
       <Card
         image='images/home/cards/facial_recognition.png'
         title='Segurança Aprimorada'
@@ -27,25 +27,21 @@ const Cards = () => {
         title='Notificações Instantâneas'
         text='A cada abastecimento, informamos o cliente em tempo real. Notificações e e-mails detalhados fornecem todas as informações pertinentes, incluindo veículo, motorista, posto e dados de abastecimento.'
       />
-
       <Card
         image='images/home/cards/invoice.png'
         title='Gerenciamento Ágil e Descomplicado'
         text='Nosso sistema facilita a gestão. Receba alertas automáticos sobre fechamentos de fatura e, com apenas alguns cliques, mantenha-se atualizado sobre o status dos pagamentos'
       />
-
       <Card
         image='images/home/cards/clients.png'
         title='Gestão de Clientes Evoluída'
         text='Gerencie seus clientes frotistas com eficiência e ofereça a eles a autonomia necessária. O cadastro de veículos e motoristas é simplificado e mantido pelos próprios clientes, otimizando seu processo.'
       />
-
       <Card
         image='images/home/cards/vehicles.png'
         title='Informações dos Veículos'
         text='Nosso sistema não só fornece a placa, mas também detalha marca, modelo e ano dos veículos, incluindo a capacidade de verificar restrição de combustíveis para cada veículo em postos específicos.'
       />
-
       <Card
         image='images/home/cards/drivers.png'
         title='Facilidade para Motoristas'
