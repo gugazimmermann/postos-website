07e5f58f49670c87844e557350f067636cffd794
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-19 14:20:03 -0300

    add react native

---
+++ app/tecnologias/ReactNative.js
@@ -0,0 +1,30 @@
+import { SectionParagraph } from '../components/layout/sections';
+
+const ReactNative = () => {
+  return (
+    <div className='px-4'>
+      <SectionParagraph>
+        Desenvolvido pelo Facebook, o React Native foi projetado para fornecer experiências nativas
+        e de alta qualidade. Ele permite aos desenvolvedores criar aplicativos móveis para iOS e
+        Android, utilizando um único código base, o que otimiza a eficiência e a velocidade de
+        desenvolvimento. Por trás do React Native estão as mentes brilhantes da equipe de engenharia
+        do Facebook, que também contribuíram para outras inovações notáveis no mundo tecnológico.
+      </SectionParagraph>
+      <SectionParagraph>
+        Empresas de renome como Instagram, Airbnb e Tesla adotaram o React Native, valorizando sua
+        flexibilidade, desempenho e eficiência no desenvolvimento multiplataforma. Além de
+        proporcionar uma experiência de usuário fluida e nativa, o React Native é aclamado por sua
+        capacidade de integrar diretamente com código nativo quando necessário, garantindo que os
+        aplicativos mantenham um alto nível de desempenho.
+      </SectionParagraph>
+      <SectionParagraph>
+        Ao escolhermos o React Native para nossas soluções móveis, estamos nos alinhando a uma
+        tecnologia comprovada e amplamente adotada por líderes da indústria. Isto garante que nossos
+        aplicativos ofereçam uma experiência de usuário superior, ágil e consistente em diversas
+        plataformas, consolidando nossa posição na vanguarda das soluções móveis modernas.
+      </SectionParagraph>
+    </div>
+  );
+};
+
+export default ReactNative;
--- app/tecnologias/Tecnologies.js
+++ app/tecnologias/Tecnologies.js
@@ -3,9 +3,11 @@
 import { useModal } from '../hooks';
 import { ModalOpen } from '../components/icons';
 import { SectionParagraph } from '../components/layout/sections';
+import { AppTitle } from '../components/general';
 import Cicd from './Cicd';
 import React from './React';
 import Amazon from './Amazon';
+import ReactNative from './ReactNative';
 import Electron from './Electron';
 import Go from './Go';
 
@@ -17,6 +19,9 @@ const Tecnologies = () => {
     closeoutside: true,
   });
   const { openModal: openAmazonModal, Modal: AmazonModal } = useModal({
+    closeoutside: true,
+  });
+  const { openModal: openReactNativeModal, Modal: ReactNativeModal } = useModal({
     closeoutside: true,
   });
   const { openModal: openElectronModal, Modal: ElectronModal } = useModal({
@@ -41,11 +46,11 @@ const Tecnologies = () => {
   return (
     <>
       <SectionParagraph>
-        Em <strong>{process.env.NEXT_PUBLIC_TITLE}</strong>, estamos comprometidos em proporcionar
-        soluções de gerenciamento de frotas que incorporam as últimas inovações tecnológicas. Nossa
-        abordagem é baseada na modernidade, visando garantir que nossos sistemas permaneçam à frente
-        do panorama tecnológico. Priorizamos oferecer acesso ágil e seguro ao nosso sistema,
-        utilizando as ferramentas mais recentes em programação web.
+        Em <AppTitle />, estamos comprometidos em proporcionar soluções de gerenciamento de frotas
+        que incorporam as últimas inovações tecnológicas. Nossa abordagem é baseada na modernidade,
+        visando garantir que nossos sistemas permaneçam à frente do panorama tecnológico.
+        Priorizamos oferecer acesso ágil e seguro ao nosso sistema, utilizando as ferramentas mais
+        recentes em programação web.
       </SectionParagraph>
       <SectionParagraph>
         Nossos esforços em{' '}
@@ -54,14 +59,16 @@ const Tecnologies = () => {
         eficaz.
       </SectionParagraph>
       <SectionParagraph>
-        Desenvolvemos nosso sistema com a tecnologia <Button text='React' onClick={openReactModal} />, que permite um acesso leve e
-        rápido de qualquer computador ou celular com conexão à internet. Não há necessidade de
-        instalação. A plataforma se adapta perfeitamente, assegurando desempenho consistente em
-        todos os dispositivos. A facilidade de acesso prioriza o gerenciamento de frotas, oferecendo
-        uma experiência incomparável.
+        Desenvolvemos nosso sistema com a tecnologia{' '}
+        <Button text='React' onClick={openReactModal} />, que permite um acesso leve e rápido de
+        qualquer computador ou celular com conexão à internet. Não há necessidade de instalação. A
+        plataforma se adapta perfeitamente, assegurando desempenho consistente em todos os
+        dispositivos. A facilidade de acesso prioriza o gerenciamento de frotas, oferecendo uma
+        experiência incomparável.
       </SectionParagraph>
       <SectionParagraph>
-        Nossa infraestrutura é completamente suportada pela <Button text='Amazon Web Services (AWS)' onClick={openAmazonModal} />, evidenciando nosso
+        Nossa infraestrutura é completamente suportada pela{' '}
+        <Button text='Amazon Web Services (AWS)' onClick={openAmazonModal} />, evidenciando nosso
         comprometimento com a segurança e integridade dos dados. A AWS proporciona um ambiente
         altamente seguro e versátil, cobrindo desde bancos de dados e armazenamento de arquivos até
         envio de e-mails e notificações. Adicionalmente, a AWS possibilita o gerenciamento seguro de
@@ -72,29 +79,31 @@ const Tecnologies = () => {
       </SectionParagraph>
       <SectionParagraph>
         Nossos aplicativos móveis são projetados priorizando a experiência do usuário, permitindo
-        gerenciamento de frotas de forma prática e eficiente, independente do sistema operacional.
+        gerenciamento de frotas de forma prática e eficiente, independente do sistema operacional,
+        para isto utilizamos <Button text='React Native' onClick={openReactNativeModal} />.
         Compatíveis com Android e iOS, os aplicativos garantem flexibilidade e facilidade de uso.
       </SectionParagraph>
       <SectionParagraph>
-        Destacamos ainda nosso Ponto de Venda (PdV) no caixa, criado com o <Button text='Electron' onClick={openElectronModal} />, uma
-        tecnologia que oferece conveniência e segurança. Diferentemente de um navegador web, nosso
-        PdV é instalado diretamente no dispositivo, proporcionando acesso direto e rápido ao
-        sistema. É projetado exclusivamente para operar nosso sistema, sem permitir navegação na
-        web, o que fortalece a segurança e mantém o foco no gerenciamento eficaz das transações.
-        Isso resulta em um ambiente controlado e produtivo para suas operações diárias.
+        Destacamos ainda nosso Ponto de Venda (PdV) no caixa, criado com o{' '}
+        <Button text='Electron' onClick={openElectronModal} />, uma tecnologia que oferece
+        conveniência e segurança. Diferentemente de um navegador web, nosso PdV é instalado
+        diretamente no dispositivo, proporcionando acesso direto e rápido ao sistema. É projetado
+        exclusivamente para operar nosso sistema, sem permitir navegação na web, o que fortalece a
+        segurança e mantém o foco no gerenciamento eficaz das transações. Isso resulta em um
+        ambiente controlado e produtivo para suas operações diárias.
       </SectionParagraph>
       <SectionParagraph>
         Além disso, nossa integração com o Desbravador é essencial em nossa gama de serviços. Esta
         integração é fundamental para transmitir de maneira eficiente e precisa os dados de
-        abastecimento nas bombas dos postos ao sistema. Usamos a linguagem de programação <Button text='Go' onClick={openGoModal} />
-        , reconhecida por sua eficiência e agilidade, garantindo uma conexão robusta entre os
-        sistemas.
+        abastecimento nas bombas dos postos ao sistema. Usamos a linguagem de programação{' '}
+        <Button text='Go' onClick={openGoModal} />, reconhecida por sua eficiência e agilidade,
+        garantindo uma conexão robusta entre os sistemas.
       </SectionParagraph>
       <SectionParagraph>
-        Em <strong>{process.env.NEXT_PUBLIC_TITLE}</strong>, acreditamos que a excelência
-        tecnológica é o alicerce do nosso sucesso. Continuamos a investir nas melhores práticas
-        tecnológicas, assegurando que nossos clientes tenham acesso a soluções inovadoras,
-        eficientes e seguras, atendendo suas necessidades em constante transformação.
+        Em <AppTitle />, acreditamos que a excelência tecnológica é o alicerce do nosso sucesso.
+        Continuamos a investir nas melhores práticas tecnológicas, assegurando que nossos clientes
+        tenham acesso a soluções inovadoras, eficientes e seguras, atendendo suas necessidades em
+        constante transformação.
       </SectionParagraph>
       <CicdModal title='Integração Contínua e Entrega Contínua' scroll={true}>
         <Cicd />
@@ -105,6 +114,9 @@ const Tecnologies = () => {
       <AmazonModal title='Amazon Web Services (AWS)' scroll={true}>
         <Amazon />
       </AmazonModal>
+      <ReactNativeModal title='React Native' scroll={true}>
+        <ReactNative />
+      </ReactNativeModal>
       <ElectronModal title='Electron' scroll={true}>
         <Electron />
       </ElectronModal>
