613e57cb76f24f2a91a08798fee72c643a675753
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-19 16:08:43 -0300

    home page techs

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -3,4 +3,5 @@ export { default as Jumbotron } from './Jumbotron';
 export { default as Parallax } from './Parallax';
 export { default as Cards } from './Cards';
 export { default as CallToAction } from './CallToAction';
+export { default as Techs } from './Techs';
 
