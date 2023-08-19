5843ca797c46dfee71e13955e8bac69bd92f3128
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-19 19:59:13 -0300

    companies page

--- app/components/layout/sections/SectionParagraph.js
+++ app/components/layout/sections/SectionParagraph.js
@@ -1,5 +1,5 @@
 const SectionParagraph = ({ children }) => {
-  return <p className='mt-4 text-slate-600'>{children}</p>;
+  return <p className='mt-4 text-slate-600 leading-relaxed'>{children}</p>;
 };
 
 export default SectionParagraph;
