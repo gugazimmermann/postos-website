dd3ed5b85d2bb095208153123e4a13992628e28d
Guga Zimmermann <gugazimmermann@gmail.com>
2023-08-20 02:38:48 -0300

    fix aria

--- app/components/layout/social/Social.js
+++ app/components/layout/social/Social.js
@@ -1,40 +1,37 @@
 import { Whatsapp, Facebook, Instagram, Threads, Twitter, Youtube, Linkedin } from '../../icons';
 
 const Social = ({ className }) => {
-  const SocialIcon = ({ name }) => {
-    let icon = null;
-    if (name === 'Whatsapp') icon = <Whatsapp />;
-    if (name === 'Facebook') icon = <Facebook />;
-    if (name === 'Instagram') icon = <Instagram />;
-    if (name === 'Threads') icon = <Threads />;
-    if (name === 'Twitter') icon = <Twitter />;
-    if (name === 'Youtube') icon = <Youtube />;
-    if (name === 'Linkedin') icon = <Linkedin />;
-    if (icon) {
-      return (
-        <div className='mx-2 text-slate-600 hover:text-amber-500 hover:cursor-pointer'>{icon}</div>
-      );
-    }
-    return null;
+
+  const icons = {
+    Whatsapp: <Whatsapp />,
+    Facebook: <Facebook />,
+    Instagram: <Instagram />,
+    Threads: <Threads />,
+    Twitter: <Twitter />,
+    Youtube: <Youtube />,
+    Linkedin: <Linkedin />
   };
 
   const SocialItem = ({ name, linkTo }) => {
+    const Icon = icons[name];
+    if (!Icon) return null;
+
     return (
       <a href={linkTo} target='_blank' rel='noopener noreferrer' aria-label={name}>
-        <SocialIcon name={name} />
+        <div className='mx-2 text-slate-600 hover:text-amber-500 hover:cursor-pointer'>{Icon}</div>
       </a>
     );
   };
 
   return (
     <div className={`flex justify-center ${className ? className : ''}`}>
-      <SocialItem name='Whatsapp' linkhref='https://wa.me/5547988704247' />
-      <SocialItem name='Facebook' linkhref='https://www.facebook.com/' />
-      <SocialItem name='Instagram' linkhref='https://www.instagram.com/' />
-      <SocialItem name='Threads' linkhref='https://www.threads.net/' />
-      <SocialItem name='Twitter' linkhref='https://www.twitter.com/' />
-      <SocialItem name='Youtube' linkhref='https://www.youtube.com/' />
-      <SocialItem name='Linkedin' linkhref='https://www.linkedin.com/' />
+      <SocialItem name='Whatsapp' linkTo='https://wa.me/5547988704247' />
+      <SocialItem name='Facebook' linkTo='https://www.facebook.com/' />
+      <SocialItem name='Instagram' linkTo='https://www.instagram.com/' />
+      <SocialItem name='Threads' linkTo='https://www.threads.net/' />
+      <SocialItem name='Twitter' linkTo='https://www.twitter.com/' />
+      <SocialItem name='Youtube' linkTo='https://www.youtube.com/' />
+      <SocialItem name='Linkedin' linkTo='https://www.linkedin.com/' />
     </div>
   );
 };
