5042e32d05ec876f35c1feef4ae04b41765d594d
Guga Zimmermann <gugazimmermann@gmail.com>
2023-09-05 14:29:57 -0300

    fix react

--- app/components/icons/Twitter.js
+++ app/components/icons/Twitter.js
@@ -2,15 +2,15 @@ const Twitter = () => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      class='icon icon-tabler icon-tabler-brand-x'
+      className='icon icon-tabler icon-tabler-brand-x'
       width='24'
       height='24'
       viewBox='0 0 24 24'
       strokeWidth='2'
       stroke='currentColor'
       fill='none'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
       <path stroke='none' d='M0 0h24v24H0z' fill='none' />
       <path d='M4 4l11.733 16h4.267l-11.733 -16z' />
