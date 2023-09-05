d12a036de4f4d235ba9ee89828c43640e5a14f03
Guga Zimmermann <gugazimmermann@gmail.com>
2023-09-04 21:29:38 -0300

    started faq

--- app/utils/faq.js
+++ app/utils/faq.js
@@ -5,38 +5,64 @@ const faq = [
     items: [
       {
         id: 1,
-        question: 'Como eu faço alguma coisa?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'O que é o Touch Sistemas Postos?',
+        text: 'O Touch Sistemas - Postos é um Sistema para Gerenciamento de Frotas para Postos de Combustíveis, nossa essência reside no controle apurado dos abastecimentos de veículos de frotas. Afinamos cada detalhe, desde a autenticação dos abastecimentos, acompanhamento de faturamento, controle granular, notificações em tempo real, relatórios detalhados, passando por agendamento de serviços até promoções personalizadas.',
       },
       {
         id: 2,
-        question: 'Como eu faço alguma coisa 2?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'É cobrado uma mensalidade?',
+        text: 'Nossa cobrança é realizada com base nos clientes ativos, ou seja, os clientes que abasteceram naquele mês (não importando a quantidade de abastecimentos realizados). Por exemplo, se 12 clientes abasteceram no mês, será cobrado somente a utilização de 12 clientes.',
       },
       {
         id: 3,
-        question: 'Como eu faço alguma coisa 3?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'Qual o valor por cliente?',
+        text: 'Trabalhamos com uma cobrança gradual, de 1 até 10 clientes o valor é R$ 50,00 Fixo, de 11 até 100 clientes será cobrado R$ 5,00 por cliente, e acima de 100 clientes será cobrado R$ 3,50 por cliente. Então, por exemplo, se 12 clientes abasteceram no mês o valor será de R$ 60,00, se 120 clientes abasteceram, o valor será de R$ 570,00 - R$ 500,00 pelos primeiros 100 clientes e R$ 70,00 pelos 20 clientes acima de 100.',
       },
       {
         id: 4,
-        question: 'Como eu faço alguma coisa 4?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'Terei alguma cobrança extra?',
+        text: 'Não! Você pagará somente por cliente ativo. Os dois únicos casos que podem ter uma cobrança diferente são as Personalizações e Novos Relatórios.',
       },
       {
         id: 5,
-        question: 'Como eu faço alguma coisa 5?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'Tenho um período de utilização mínima?',
+        text: 'Não, vocês pode parar de utilizar no momento que desejar. Se não tiver nenhum cliente ativo, não terá nenhuma cobrança.',
       },
       {
         id: 6,
-        question: 'Como eu faço alguma coisa 6?',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum quae. Harum officiis reprehenderit ex quia ducimus minima id provident molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.',
+        question: 'O que preciso para começar a utilizar?',
+        text: 'Precisa somente clicar em Cadastro, ali em cima, e preencher os dados da sua organização. Feito isto é cadastrar seu posto(s) e convidar seus clientes.',
       },
     ],
   },
   {
     id: 2,
+    name: 'Organizações',
+    items: [
+      {
+        id: 1,
+        question: 'O que é a Organização?',
+        text: 'Dentro do Touch Sistemas Postos uma Organização é a detentora de um ou mais postos, ela é o topo da piramide de controle do sistema.',
+      },
+      {
+        id: 2,
+        question: 'Quantos postos posso cadastrar?',
+        text: 'Não existe um limite. Nosso sistema foi desenvolvido para atender qualquer demanda, e para isto utilizamos o que tem de mais moderno em tecnologia nas nuvens.',
+      },
+      {
+        id: 3,
+        question: 'Posso controlar os postos pela organização?',
+        text: 'Sim, como topo no controle do sistema, o usuário da organização de controle sobre todos os postos. E quando a organização torna inativo um cliente, motorista ou veículo, o mesmo não pode ser ativado novamente por nenhum posto até a organização tornar o mesmo ativo novamente.',
+      },
+      {
+        id: 4,
+        question: 'Como ter relatórios consolidados?',
+        text: 'Acessando o sistema como uma organização automaticamente você terá acesso ao dashboard e relatórios consolidados, de todos os postos, e também poderá navegar para ver os gráficos e tabelas individuais de cada posto, cliente, etc.',
+      },
+    ],
+  },
+  {
+    id: 3,
     name: 'Postos',
     items: [
       {
@@ -47,7 +73,7 @@ const faq = [
     ],
   },
   {
-    id: 3,
+    id: 4,
     name: 'Clientes',
     items: [
       {
@@ -58,7 +84,7 @@ const faq = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     name: 'Abastecimentos',
     items: [
       {
@@ -69,7 +95,7 @@ const faq = [
     ],
   },
   {
-    id: 5,
+    id: 6,
     name: 'Faturamentos',
     items: [
       {
